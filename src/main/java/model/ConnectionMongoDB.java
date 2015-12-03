f74e79571a1d33a51720baa0dfa24f24af0d88b4
ChiChi <chironromain@gmail.com>
2015-12-03 13:27:32 +0100

    Connection MongoDB et première requête

--- src/main/java/model/ConnectionMongoDB.java
+++ src/main/java/model/ConnectionMongoDB.java
@@ -1,17 +1,36 @@
 package model;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.BsonDocument;
+import org.bson.Document;
 
-/**
- * Created by Corentin on 03/12/2015.
- */
+import javax.swing.*;
+import java.time.temporal.JulianFields;
+import java.util.Arrays;
+import java.util.Date;
+
+
+
 public class ConnectionMongoDB
 {
     public static void main( String args[] )
     {
-        MongoClientURI connectionString;
-        connectionString = new MongoClientURI("mongodb://localhost:27017");
+        MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
         MongoClient mongoClient = new MongoClient(connectionString);
+        MongoDatabase database = mongoClient.getDatabase("projet");
+
+        MongoCollection<Document> collection = database.getCollection("movies");
+
+        FindIterable<Document> iterable = collection.find(
+                new Document("fields.year", 2015));
+        iterable.forEach(new Block<Document>() {
+            @Override
+            public void apply(final Document document) {
+                System.out.println(document);
+            }
+        });
     }
 }
