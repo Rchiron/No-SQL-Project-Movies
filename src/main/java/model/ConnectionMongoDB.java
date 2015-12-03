2d3399f2932830c69509ad4d6e2036cfd940a8cc
Corentin LEPRAEL <corentin.leprael@devinci.fr>
2015-12-03 11:52:42 +0100

    class ConnectionMongoDB

--- src/main/java/model/ConnectionMongoDB.java
+++ src/main/java/model/ConnectionMongoDB.java
@@ -1,7 +1,17 @@
 package model;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
 /**
  * Created by Corentin on 03/12/2015.
  */
-public class ConnectionMongoDB {
+public class ConnectionMongoDB
+{
+    public static void main( String args[] )
+    {
+        MongoClientURI connectionString;
+        connectionString = new MongoClientURI("mongodb://localhost:27017");
+        MongoClient mongoClient = new MongoClient(connectionString);
+    }
 }
